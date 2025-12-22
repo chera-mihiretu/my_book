b455e5053f772325ee3536e05c9bc9f0d7da6bff
chera-mihiretu <cheramihiretucm@gmail.com>
2025-12-22 14:20:35 +0300

    Update AuthorDetailRepository implementation

--- lib/features/author_detail/data/repositories/author_detail_repository_impl.dart
+++ lib/features/author_detail/data/repositories/author_detail_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/error/exceptions.dart';
 import '../../../../core/error/failures.dart';
 import '../../../../core/models/author_detail_model.dart';
+import '../../../../core/models/work_model.dart';
 import '../../domain/repositories/author_detail_repository.dart';
 import '../datasources/author_detail_remote_data_source.dart';
 
@@ -19,6 +20,20 @@ class AuthorDetailRepositoryImpl implements AuthorDetailRepository {
       return Right(authorDetail);
     } on ServerException {
       return const Left(ServerFailure('Failed to fetch author details'));
+    } catch (e) {
+      return const Left(ServerFailure('An unexpected error occurred'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<WorkModel>>> getAuthorWorks(
+    String authorKey,
+  ) async {
+    try {
+      final works = await remoteDataSource.getAuthorWorks(authorKey);
+      return Right(works);
+    } on ServerException {
+      return const Left(ServerFailure('Failed to fetch author works'));
     } catch (e) {
       return const Left(ServerFailure('An unexpected error occurred'));
     }
