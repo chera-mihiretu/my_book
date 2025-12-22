eb9dff1d202045e403c5adeaf3ecfc6ec5a05b29
chera-mihiretu <cheramihiretucm@gmail.com>
2025-12-22 14:15:14 +0300

    Improve AuthorDetailRemoteDataSource

--- lib/features/author_detail/data/datasources/author_detail_remote_data_source.dart
+++ lib/features/author_detail/data/datasources/author_detail_remote_data_source.dart
@@ -1,11 +1,15 @@
 import 'dart:convert';
+import 'dart:developer';
+
 import 'package:http/http.dart' as http;
 import '../../../../core/error/exceptions.dart';
 import '../../../../core/models/author_detail_model.dart';
+import '../../../../core/models/work_model.dart';
 import '../../../../core/utils/constants.dart';
 
 abstract class AuthorDetailRemoteDataSource {
   Future<AuthorDetailModel> getAuthorDetail(String authorKey);
+  Future<List<WorkModel>> getAuthorWorks(String authorKey);
 }
 
 class AuthorDetailRemoteDataSourceImpl implements AuthorDetailRemoteDataSource {
@@ -23,6 +27,24 @@ class AuthorDetailRemoteDataSourceImpl implements AuthorDetailRemoteDataSource {
     if (response.statusCode == 200) {
       final Map<String, dynamic> jsonMap = json.decode(response.body);
       return AuthorDetailModel.fromJson(jsonMap);
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<WorkModel>> getAuthorWorks(String authorKey) async {
+    log(ApiEndpoints.authorWorks(authorKey));
+
+    final response = await client.get(
+      Uri.parse(ApiEndpoints.authorWorks(authorKey)),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonMap = json.decode(response.body);
+      final List<dynamic> entries = jsonMap['entries'] ?? [];
+      return entries.map((e) => WorkModel.fromJson(e)).toList();
     } else {
       throw ServerException();
     }
