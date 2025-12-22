2e4b2331320c2304e40b9b5097008d05e20796cc
chera-mihiretu <cheramihiretucm@gmail.com>
2025-12-22 14:24:50 +0300

    Update AuthorDetailRepository interface

--- lib/features/author_detail/domain/repositories/author_detail_repository.dart
+++ lib/features/author_detail/domain/repositories/author_detail_repository.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import '../../../../core/error/failures.dart';
 import '../../../../core/models/author_detail_model.dart';
+import '../../../../core/models/work_model.dart';
 
 abstract class AuthorDetailRepository {
   Future<Either<Failure, AuthorDetailModel>> getAuthorDetail(String authorKey);
+  Future<Either<Failure, List<WorkModel>>> getAuthorWorks(String authorKey);
 }
