94f2e11bdaf20b5784d4e9dc920ce98a94786ff0
chera-mihiretu <cheramihiretucm@gmail.com>
2025-12-24 15:26:13 +0300

    Update NotificationRepository interface

--- lib/features/notification_settings/domain/repositories/notification_repository.dart
+++ lib/features/notification_settings/domain/repositories/notification_repository.dart
@@ -7,4 +7,5 @@ abstract class NotificationRepository {
   Future<Either<Failure, NotificationSettingsModel>> updateNotificationSettings(
     bool enabled,
   );
+  Future<Either<Failure, NotificationSettingsModel>> saveToken(String token);
 }
