3f4579f122805c0c7959e90852da631cc26c090c
chera-mihiretu <cheramihiretucm@gmail.com>
2025-12-24 15:41:01 +0300

    Enhance NotificationBloc

--- lib/features/notification_settings/presentation/bloc/notification_bloc.dart
+++ lib/features/notification_settings/presentation/bloc/notification_bloc.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:new_project/features/notification_settings/data/models/notification_settings_model.dart';
 import '../../domain/usecases/get_notification_settings_usecase.dart';
+import '../../domain/usecases/save_notification_token_usecase.dart';
 import '../../domain/usecases/update_notification_settings_usecase.dart';
 import 'notification_event.dart';
 import 'notification_state.dart';
@@ -7,13 +9,16 @@ import 'notification_state.dart';
 class NotificationBloc extends Bloc<NotificationEvent, NotificationState> {
   final GetNotificationSettingsUseCase getNotificationSettingsUseCase;
   final UpdateNotificationSettingsUseCase updateNotificationSettingsUseCase;
+  final SaveNotificationTokenUseCase saveNotificationTokenUseCase;
 
   NotificationBloc({
     required this.getNotificationSettingsUseCase,
     required this.updateNotificationSettingsUseCase,
+    required this.saveNotificationTokenUseCase,
   }) : super(const NotificationInitial()) {
     on<LoadNotificationSettingsEvent>(_onLoadNotificationSettings);
     on<UpdateNotificationSettingsEvent>(_onUpdateNotificationSettings);
+    on<NotificationTokenSave>(_onSaveNotificationToken);
   }
 
   Future<void> _onLoadNotificationSettings(
@@ -27,6 +32,22 @@ class NotificationBloc extends Bloc<NotificationEvent, NotificationState> {
     result.fold(
       (failure) => emit(NotificationError(failure.message)),
       (settings) => emit(NotificationLoaded(settings)),
+    );
+  }
+
+  Future<void> _onSaveNotificationToken(
+    NotificationTokenSave event,
+    Emitter<NotificationState> emit,
+  ) async {
+    emit(const NotificationLoading());
+
+    final result = await saveNotificationTokenUseCase(event.token);
+
+    result.fold(
+      (failure) => emit(NotificationError(failure.message)),
+      (settings) => emit(
+        NotificationUpdated(settings, 'Notification token saved successfully'),
+      ),
     );
   }
 
